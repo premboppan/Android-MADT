f522ba5e087a9af97b4cdc6d8b57080e18800d59
MacStudent <macstudent@20X-iMacXX-M-386.local>
2018-07-04 17:10:43 -0400

    Committed

---
+++ app/src/main/java/com/example/macstudent/myapplication/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.macstudent.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Thread myThread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+
+
+                }
+            }
+            };
+                myThread.start();
+
+            }
+        }
+
