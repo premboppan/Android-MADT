43142f7280e4819af8aedcbd3fd1161adfc0bfdf
premboppan <premsaiboppana@fgmail.com>
2018-07-09 21:44:05 -0400

    Committed

---
+++ app/src/main/java/com/example/macstudent/myapplication/SQliteOpenHelper.java
@@ -0,0 +1,8 @@
+package com.example.macstudent.myapplication;
+
+/**
+ * Created by ADMIN on 7/9/2018.
+ */
+
+class SQliteOpenHelper {
+}
---
+++ app/src/test/java/com/example/macstudent/myapplication/DatabaseHelperTest.java
@@ -0,0 +1,10 @@
+package com.example.macstudent.myapplication;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by ADMIN on 7/9/2018.
+ */
+public class DatabaseHelperTest extends SQliteOpenHelper{
+
+}
